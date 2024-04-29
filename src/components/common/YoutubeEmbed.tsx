f564cda1a215f414d1828efca2c17b63e1570620
Rahul Desar <desarrahul@gmail.com>
2024-04-29 13:44:54 +0545

    add caroused and youtube embed modal

---
+++ src/components/common/YoutubeEmbed.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+// videoId, title, ButtonDescription
+const YouTubeModal = ({ videoId, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative">
+      <div className="absolute z-10">
+        <button
+          className="card-title font-bold text-2xl uppercase text-white hover:-translate-y-2 ease-in-out duration-100 text-shadow-sm"
+          onClick={openModal}
+        >
+          {children}
+        </button>
+      </div>
+      {isOpen && (
+        <div
+          className="fixed inset-0 h-full flex items-center justify-center z-50 bg-black bg-opacity-75"
+          onClick={closeModal}
+        >
+          <div className="relative w-11/12 max-w-6xl h-auto">
+            <div className="bg-white p-6">
+              <div className="aspect-video">
+                <iframe
+                  className="absolute inset-0 w-full h-full"
+                  src={`https://www.youtube.com/embed/${videoId}`}
+                  title="YouTube Embed Video"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default YouTubeModal;
